b5b61cadfc704da15116ef7cbe0ecef14fc823c6
Madhur Jain <madhurjain@Madhurs-Mac-mini.local>
2021-05-11 21:09:31 +0530

    latest

--- src/Components/Cart/CartDropDown.jsx
+++ src/Components/Cart/CartDropDown.jsx
@@ -1,22 +1,28 @@
 import React from "react";
 import "../../CSS/Cart/cartDropDown.scss";
+import CustomButton from "../CustomButton";
 function CartDropDown({ cartItems }) {
   return (
-    <div className="cartdropdown-wrapper">
+    <div className="cartdropdown">
       {cartItems && cartItems.length > 0 ? (
-        cartItems.map((item) => {
-          return (
-            <div key={item.id} className="cartdropdown-cartItems">
-              <img src={item.imageUrl} />
-              <div>
-                <span>{item.name}</span>
-                <span>
-                  $ {item.price} X {item.quantity}
-                </span>
+        <div className="cartdropdown-wrapper">
+          {cartItems.map((item) => {
+            return (
+              <div key={item.id} className="cartdropdown-cartItems">
+                <img src={item.imageUrl} />
+                <div>
+                  <span>{item.name}</span>
+                  <span>
+                    $ {item.price} X {item.quantity}
+                  </span>
+                </div>
               </div>
-            </div>
-          );
-        })
+            );
+          })}
+          <div>
+            <CustomButton matter="CHECKOUT" type="button" />
+          </div>
+        </div>
       ) : (
         <span>You have no cart Items</span>
       )}
